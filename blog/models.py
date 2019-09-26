96ff7708302a063ccb320342fb170b50a328ac6d
thomashhs <345870016@qq.com>
2019-09-27 07:39:52 +0800

    20190927-1

---
+++ blog/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from users.models import User
+from django.urls import reverse
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        ordering = ['-created_time']
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
