7ee1a1116118ef46add85289e4da34f906902953
thomashhs <345870016@qq.com>
2019-09-28 00:36:51 +0800

    20190928-1

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,38 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from .models import Post,Category
+from .models import Post,Category,Tag
 import markdown
 from comments.forms import CommentForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
 
 # Create your views here.
 def index(request):
     post_list=Post.objects.all().order_by('-created_time')
-    return render(request,'blog/index.html',context={'post_list':post_list})
+    paginator = Paginator(post_list, 2)
+    page = request.GET.get('page')
+
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
+
+    return render(request,'blog/index.html',context={'post_list':post_list,
+                                                     'paginator':paginator})
 
 def detail(request,pk):
     post=get_object_or_404(Post,pk=pk)
     post.increase_views()
-    post.body=markdown.markdown(post.body,
-                                extensions=[
+    md=markdown.Markdown(extensions=[
                                     'markdown.extensions.extra',
                                     'markdown.extensions.codehilite',
-                                    'markdown.extensions.toc',
+                                     TocExtension(slugify=slugify),
                                 ])
+    post.body=md.convert(post.body)
+    post.toc=md.toc
     form=CommentForm()
     comment_list = post.comment_set.all()
     context = {'post': post,
@@ -35,4 +50,9 @@ def archives(request, year, month):
 def category(request,pk):
     cate = get_object_or_404(Category, pk=pk)
     post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def tag(request,pk):
+    tag = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=tag).order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
